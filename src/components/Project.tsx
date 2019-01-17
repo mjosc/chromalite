a503e1a8420ed784b38bb82ff238bcbd8c13cdcd
Matt Josse <matt.m.josse@gmail.com>
2019-01-17 00:21:29 -0700

    In process of calculating scheme height internally

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,38 +3,53 @@ import * as React from 'react';
 import ProjectMenu from './ProjectMenu';
 import './Project.css';
 
-import { database as db } from '../database';
+import { database as db, SchemeCollection, Scheme } from '../database';
 
 interface Props {
 
 }
 
 interface State {
-
+  schemesWidth: number
+  schemes: SchemeCollection
 }
 
-class Project extends React.Component<{}, {}> {
+class Project extends React.Component<{}, State> {
+
+  private schemesRef = React.createRef<HTMLDivElement>();
+
   constructor(props: {}) {
     super(props);
     this.state = {
-      
+      schemesWidth: 0,
+      schemes: []
     }
   }
-  componentDidMount() {
-    db.post('api/hello', ['#456789'])
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err.response.data));
+
+  componentWillMount() {
     db.get('api/schemes')
-      .then(res => console.log(res))
+      .then(res => this.setState({ schemes: res.data as SchemeCollection }))
       .catch(err => console.log(err.response.data))
   }
+  
   render() {
     return (
       <div className='Project'>
         <ProjectMenu />
-        <div className='Project_schemes' />
+        <div className='Project__schemes' ref={this.schemesRef}>
+          {this.state.schemes.map((scheme, index) => (
+            <div className='Project__scheme' style={{ width: '25em' }}>
+              {scheme.map((color, idx, arr) => (
+                <div
+                  className='Project__color'
+                  style={{ width: `${400 / arr.length}em`, backgroundColor: color }}
+                >test</div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
 
