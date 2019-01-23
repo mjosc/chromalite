c6c4f68cd4c101463f3500af573b8156e3410cd8
Matt Josse <matt.m.josse@gmail.com>
2019-01-23 15:54:22 -0700

    Refactored to use destructuring

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -4,18 +4,29 @@ import './LoadingAnimation.css'
 
 interface Props {
   color?: string,
-  width?: string,
-  height?: string
+  width?: number | string,
+  height?: number | string,
+  classes?: { external: string, internal: string }
 }
 
-export default (props: Props) => (
-  <div className='LoadingAnimation' >
-    <ReactLoading
-      className='LoadingAnimation__internal'
-      type={'spin'}
-      color={props.color}
-      width={props.width}
-      height={props.height}
-    />
-  </div >
-);
+const LoadingAnimation: React.FunctionComponent<Props> = props => {
+  const { classes, ...rest } = props;
+  return (
+    <div className={`LoadingAnimation ${classes!.external}`} >
+      <ReactLoading
+        className={`LoadingAnimation__internal ${classes!.internal}`}
+        type='spin'
+        {...rest}
+      />
+    </div >
+  );
+};
+
+// ReactLoading contains its own default props. Those properties specified here
+// represent the corresponding overrides.
+LoadingAnimation.defaultProps = {
+  color: '#28425C',
+  classes: { external: '', internal: '' }
+};
+
+export default LoadingAnimation;
